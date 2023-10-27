60eeab42800ee17ef24b781a53c897bcb82336ec
t-powers <127955989+t-powers@users.noreply.github.com>
2023-10-26 20:56:29 -0400

    added first score function

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let one = document.getElementsByClassName("onePoint")
+let scoreHome = document.getElementById("scoreHome");
+let scoreGuest = document.getElementById("scoreGuest");
+let homeCount = 0;
+let guestCount = 0;
 
+function homeOne() {
+  homeCount += 1;
+  scoreHome.innerText = homeCount;
+}
 
-function btnOne(){
-
+function guestOne() {
+  guestCount += 1;
+  scoreGuest.innerText = guestCount;
 }
